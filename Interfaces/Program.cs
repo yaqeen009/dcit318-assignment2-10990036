d47bc3575b507010ee978d3fce561be949657d46
Yaqeen Mumuni <yaqeens@Yaqeens-Mac.local>
2024-06-17 00:48:50 +0000

    Question 3

---
+++ Interfaces/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+interface IMovable
+{
+    void Move();
+}
+
+class Car : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Car is moving");
+    }
+}
+
+class Bicycle : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Bicycle is moving");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IMovable myCar = new Car();
+        IMovable myBicycle = new Bicycle();
+
+        myCar.Move();      
+        myBicycle.Move();  
+    }
+}
