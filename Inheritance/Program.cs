c059200339f5a958936162f8be55a7ec405636ed
Yaqeen Mumuni <yaqeens@Yaqeens-Mac.local>
2024-06-17 00:25:49 +0000

    Question 1

---
+++ Inheritance/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Animal
+{
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("Make some sound");
+    }
+}
+
+class Dog : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Bark");
+    }
+}
+
+class Cat : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Meow");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Animal myAnimal = new Animal();
+        Dog myDog = new Dog();
+        Cat myCat = new Cat();
+
+        myAnimal.MakeSound(); 
+        myDog.MakeSound();   
+        myCat.MakeSound();   
+    }
+}
