dae034d140651d5b0b11dcb13fc8c8c83da04f52
Yaqeen Mumuni <yaqeens@Yaqeens-Mac.local>
2024-06-17 00:38:25 +0000

    Question 2

---
+++ AbstractClasses/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+abstract class Shape
+{
+    public abstract double GetArea();
+}
+
+class Circle : Shape
+{
+    private double radius;
+
+    public Circle(double radius)
+    {
+        this.radius = radius;
+    }
+
+    public override double GetArea()
+    {
+        return Math.PI * radius * radius;
+    }
+}
+
+class Rectangle : Shape
+{
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height)
+    {
+        this.width = width;
+        this.height = height;
+    }
+
+    public override double GetArea()
+    {
+        return width * height;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Shape myCircle = new Circle(5.0);
+        Shape myRectangle = new Rectangle(4.0, 6.0);
+
+        Console.WriteLine("Area of the circle: " + myCircle.GetArea());
+        Console.WriteLine("Area of the rectangle: " + myRectangle.GetArea());
+    }
+}
